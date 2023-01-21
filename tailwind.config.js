0417baaf63a73aab758b6e6a0b4ec311a5f9c222
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-01-21 23:38:21 +0330

    Added font to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans : ["Roboto" , "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }

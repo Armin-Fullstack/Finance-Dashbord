3323f6e5e803e53ab1f2cbdd64249eafda508561
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-01-26 21:50:05 +0330

    Added the other progress bar

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const number1 = document.querySelector(".number1")
+const number2 = document.querySelector(".number2")
 
-let counter = 0;
+let counter1 = 0;
 
 setInterval(() => {
-    if(counter === 65){
+    if(counter1 === 65){
         clearInterval
     } else {
-        counter += 1
-        number1.innerHTML = `${counter}%`
+        counter1 += 1
+        number1.innerHTML = `${counter1}%`
     }
 
 } , 30)
+
+let counter2 = 0
+setInterval(() => {
+    if(counter2 === 48){
+        clearInterval
+    } else {
+        counter2 += 1
+        number2.innerHTML = `${counter2}%`
+    }
+
+} , 35)
+
